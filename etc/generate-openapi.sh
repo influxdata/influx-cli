b876ca8b75f13042d587bfc7458e96771ce15f37
Dan Moran <dmoran@influxdata.com>
2021-04-19 15:26:54 -0400

    chore: remove remaining bash hack
    It will be replaced by follow-up edits to the template files

--- etc/generate-openapi.sh
+++ etc/generate-openapi.sh
@@ -16,29 +16,13 @@ docker run --rm -it -u "$(id -u):$(id -g)" \
   -t /influx/internal/api/templates \
   --additional-properties packageName=api,enumClassPrefix=true,generateInterfaces=true
 
-# Clean up files we don't care about.
-(
-  cd "${ROOT_DIR}/internal/api"
-  rm -rf go.mod go.sum git_push.sh api docs .openapi-generator .travis.yml .gitignore
-)
-
 # Edit the generated files.
 (
-  # Replace all uses of int32 with int64 in our generated models.
-  #
-  # See https://github.com/OpenAPITools/openapi-generator/issues/9280 for a feature request
-  # to make the Go generator's number-handling compliant with the spec, so we can generate int64
-  # fields directly without making our public docs invalid.
-  for m in internal/api/model_*.go; do
-    sed -i.bak -e 's/int32/int64/g' ${m}
-  done
+  # Clean up files we don't care about.
+  cd "${ROOT_DIR}/internal/api"
+  rm -rf go.mod go.sum git_push.sh api docs .openapi-generator .travis.yml .gitignore
 
-  rm internal/api/*.bak
-)
-
-# Since we deleted the generated go.mod, run `go mod tidy` to update parent dependencies.
-(
+  # Clean up the generated code.
   cd "${ROOT_DIR}"
   make fmt
-  go mod tidy
 )
