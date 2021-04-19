42cca33313227b5409f26f0ac7df894f17d538ef
Dan Moran <dmoran@influxdata.com>
2021-04-19 13:58:48 -0400

    refactor: replace some bash hacks with custom templates

--- etc/generate-openapi.sh
+++ etc/generate-openapi.sh
@@ -13,6 +13,7 @@ docker run --rm -it -u "$(id -u):$(id -g)" \
   -g go \
   -i /influx/internal/api/api.yml \
   -o /influx/internal/api \
+  -t /influx/internal/api/templates \
   --additional-properties packageName=api,enumClassPrefix=true,generateInterfaces=true
 
 # Clean up files we don't care about.
@@ -23,29 +24,6 @@ docker run --rm -it -u "$(id -u):$(id -g)" \
 
 # Edit the generated files.
 (
-  cd "${ROOT_DIR}"
-
-  # Inject linter directives into generated files to make staticcheck happy.
-  cat <<EOF > internal/api/client.go
-//lint:file-ignore ST1005 Ignore capitalized errors, they're generated
-//lint:file-ignore SA6005 Ignore old-fashioned way of comparing strings, it's generated
-
-$(cat internal/api/client.go)
-EOF
-  cat <<EOF > internal/api/configuration.go
-//lint:file-ignore ST1005 Ignore capitalized errors, they're generated
-
-$(cat internal/api/configuration.go)
-EOF
-
-  # Remove the OAuth code from the generated files.
-  # We don't use OAuth, and it pulls in a huge dependency on the Google Cloud libraries.
-  #
-  # See https://github.com/OpenAPITools/openapi-generator/issues/9281 for a feature request
-  # to add a toggle that would prevent OAuth code from even being generated.
-  sed -i.bak -e '/OAuth2 authentication/,+10d' -e 's#"golang.org/x/oauth2"##' internal/api/client.go
-  sed -i.bak -e '/OAuth2/,+2d' internal/api/configuration.go
-
   # Replace all uses of int32 with int64 in our generated models.
   #
   # See https://github.com/OpenAPITools/openapi-generator/issues/9280 for a feature request
