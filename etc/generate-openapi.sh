0ba329f287d26b94e9f28c74a2523fe996c18244
Dan Moran <dmoran@influxdata.com>
2021-05-16 16:33:57 -0400

    refactor: move submodule, add docs

--- etc/generate-openapi.sh
+++ etc/generate-openapi.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 declare -r ETC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 declare -r ROOT_DIR="$(dirname ${ETC_DIR})"
-declare -r CODEGEN_TARGET_DIR="${ROOT_DIR}/internal/api"
+declare -r API_DIR="${ROOT_DIR}/internal/api"
 
 declare -r GENERATED_PATTERN='^// Code generated .* DO NOT EDIT\.$'
 declare -r MERGE_DOCKER_IMG=quay.io/influxdb/swagger-cli
 declare -r GENERATOR_DOCKER_IMG=openapitools/openapi-generator-cli:v5.1.0
 
 # Clean up all the generated files in the target directory.
-rm $(grep -Elr "${GENERATED_PATTERN}" "${CODEGEN_TARGET_DIR}")
+rm $(grep -Elr "${GENERATED_PATTERN}" "${API_DIR}")
 
 # Merge all API contracts into a single file to drive codegen.
 docker run --rm -it -u "$(id -u):$(id -g)" \
-  -v "${ROOT_DIR}":/influx \
+  -v "${API_DIR}":/api \
   ${MERGE_DOCKER_IMG} \
-  swagger-cli bundle /influx/api-contract/cli.yml \
-  --outfile /influx/internal/api/cli.yml \
+  swagger-cli bundle /api/contract/cli.yml \
+  --outfile /api/cli.yml \
   --type yaml
 
 # Run the generator - This produces many more files than we want to track in git.
 docker run --rm -it -u "$(id -u):$(id -g)" \
-  -v "${CODEGEN_TARGET_DIR}":/api \
+  -v "${API_DIR}":/api \
   ${GENERATOR_DOCKER_IMG} \
   generate \
   -g go \
@@ -33,7 +34,7 @@ docker run --rm -it -u "$(id -u):$(id -g)" \
 # Edit the generated files.
 (
   # Clean up files we don't care about.
-  cd "${CODEGEN_TARGET_DIR}"
+  cd "${API_DIR}"
   rm -rf go.mod go.sum git_push.sh api docs .openapi-generator .travis.yml .gitignore
 
   # Change extension of generated files.
