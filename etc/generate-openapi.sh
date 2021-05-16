4b355adc3155dc0b4b0d38aa65b885947484ae77
Dan Moran <dmoran@influxdata.com>
2021-05-16 13:08:00 -0400

    refactor: move CLI-specific openapi YMLs into the CLI repo

--- etc/generate-openapi.sh
+++ etc/generate-openapi.sh
@@ -2,23 +2,26 @@
 
 declare -r ETC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 declare -r ROOT_DIR="$(dirname ${ETC_DIR})"
-declare -r API_DIR="${ROOT_DIR}/internal/api"
+declare -r CODEGEN_TARGET_DIR="${ROOT_DIR}/internal/api"
 
 declare -r GENERATED_PATTERN='^// Code generated .* DO NOT EDIT\.$'
+declare -r MERGE_DOCKER_IMG=quay.io/influxdb/swagger-cli
 declare -r GENERATOR_DOCKER_IMG=openapitools/openapi-generator-cli:v5.1.0
-declare -r OPENAPI_COMMIT=dd675843404dbb3881f4d245581b916df319091f
 
 # Clean up all the generated files in the target directory.
-rm $(grep -Elr "${GENERATED_PATTERN}" "${API_DIR}")
+rm $(grep -Elr "${GENERATED_PATTERN}" "${CODEGEN_TARGET_DIR}")
 
-# Download our target API spec.
-# NOTE: openapi-generator supports HTTP references to API docs, but using that feature
-# causes the host of the URL to be injected into the base paths of generated code.
-curl -o "${API_DIR}/cli.yml" https://raw.githubusercontent.com/influxdata/openapi/${OPENAPI_COMMIT}/contracts/cli.yml
+# Merge all API contracts into a single file to drive codegen.
+docker run --rm -it -u "$(id -u):$(id -g)" \
+  -v "${ROOT_DIR}":/influx \
+  ${MERGE_DOCKER_IMG} \
+  swagger-cli bundle /influx/api-contract/cli.yml \
+  --outfile /influx/internal/api/cli.yml \
+  --type yaml
 
 # Run the generator - This produces many more files than we want to track in git.
 docker run --rm -it -u "$(id -u):$(id -g)" \
-  -v "${API_DIR}":/api \
+  -v "${CODEGEN_TARGET_DIR}":/api \
   ${GENERATOR_DOCKER_IMG} \
   generate \
   -g go \
@@ -30,7 +33,7 @@ docker run --rm -it -u "$(id -u):$(id -g)" \
 # Edit the generated files.
 (
   # Clean up files we don't care about.
-  cd "${API_DIR}"
+  cd "${CODEGEN_TARGET_DIR}"
   rm -rf go.mod go.sum git_push.sh api docs .openapi-generator .travis.yml .gitignore
 
   # Change extension of generated files.
