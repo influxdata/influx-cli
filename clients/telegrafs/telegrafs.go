3f2f15ae39548192c02607ad52723428f18da777
DStrand1 <dane@strandboge.com>
2021-06-14 13:37:36 -0500

    feat: reimplement `influx telegrafs` with new APIs

---
+++ clients/telegrafs/telegrafs.go
@@ -0,0 +1,173 @@
+package telegrafs
+
+import (
+	"context"
+	"errors"
+	"io/ioutil"
+	"os"
+
+	"github.com/influxdata/influx-cli/v2/api"
+	"github.com/influxdata/influx-cli/v2/clients"
+	"github.com/influxdata/influx-cli/v2/pkg/influxid"
+)
+
+type Client struct {
+	clients.CLI
+	api.TelegrafsApi
+}
+
+type TelegrafParams struct {
+	clients.OrgParams
+
+	Desc string
+	File string
+	Name string
+
+	Id string
+	Ids []string
+}
+
+type telegrafPrintOpts struct {
+	graf  *api.Telegraf
+	grafs *api.Telegrafs
+}
+
+func (c Client) Telegrafs(ctx context.Context, params *TelegrafParams) error {
+	if params.OrgID == 0 || params.Id == "" {
+		return errors.New("at least one of org, org-id, or id must be provided")
+	}
+
+	if params.Id != "" {
+		id, err := influxid.IDFromString(params.Id)
+		if err != nil {
+			return err
+		}
+
+		telegraf, err := c.GetTelegrafsID(ctx, id.String()).Execute()
+		if err != nil {
+			return err
+		}
+
+		return c.writeTelegrafConfigs(telegrafPrintOpts{graf: &telegraf})
+	}
+
+	telegrafs, err := c.GetTelegrafs(ctx).OrgID(params.OrgID.String()).Execute()
+	if err != nil {
+		return err
+	}
+
+	return c.writeTelegrafConfigs(telegrafPrintOpts{grafs: &telegrafs})
+}
+
+func (c Client) Create(ctx context.Context, params *TelegrafParams) error {
+	cfg, err := c.readConfig(params.File)
+	if err != nil {
+		return err
+	}
+
+	orgID := params.OrgID.String()
+
+	newTelegraf := api.TelegrafRequest{
+		Name:        &params.Name,
+		Description: &params.Desc,
+		Config:      &cfg,
+		OrgID:       &orgID,
+	}
+
+	graf, err := c.PostTelegrafs(ctx).TelegrafRequest(newTelegraf).Execute()
+	if err != nil {
+		return err
+	}
+
+	return c.writeTelegrafConfigs(telegrafPrintOpts{graf: &graf})
+}
+
+func (c Client) Remove(ctx context.Context, params *TelegrafParams) error {
+	for _, rawID := range params.Ids {
+		id, err := influxid.IDFromString(rawID)
+		if err != nil {
+			return err
+		}
+
+		if err = c.DeleteTelegrafsID(ctx, id.String()).Execute(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c Client) Update(ctx context.Context, params *TelegrafParams) error {
+	cfg, err := c.readConfig(params.File)
+	if err != nil {
+		return err
+	}
+
+	id, err := influxid.IDFromString(params.Id)
+	if err != nil {
+		return err
+	}
+
+	orgID := params.OrgID.String()
+	updateTelegraf := api.TelegrafRequest{
+		Name:        &params.Name,
+		Description: &params.Desc,
+		Config:      &cfg,
+		OrgID:       &orgID,
+	}
+
+	graf, err := c.PutTelegrafsID(ctx, id.String()).TelegrafRequest(updateTelegraf).Execute()
+	if err != nil {
+		return err
+	}
+
+	return c.writeTelegrafConfigs(telegrafPrintOpts{graf: &graf})
+}
+
+func (c Client) writeTelegrafConfigs(opts telegrafPrintOpts) error {
+	if c.PrintAsJSON {
+		var v interface{}
+		if opts.graf != nil {
+			v = opts.graf
+		} else {
+			v = opts.grafs
+		}
+		return c.PrintJSON(v)
+	}
+
+	headers := []string{"ID", "OrgID", "Name", "Description"}
+
+	telegrafs := opts.grafs.GetConfigurations()
+	if opts.graf != nil {
+		telegrafs = append(telegrafs, *opts.graf)
+	}
+
+	var rows []map[string]interface{}
+	for _, u := range telegrafs {
+		row := map[string]interface{}{
+			"ID":          u.GetId(),
+			"OrgID":       u.GetOrgID(),
+			"Name":        u.GetName(),
+			"Description": u.GetDescription(),
+		}
+		rows = append(rows, row)
+	}
+
+	return c.PrintTable(headers, rows...)
+}
+
+func (c Client) readConfig(file string) (string, error) {
+	if file != "" {
+		bb, err := ioutil.ReadFile(file)
+		if err != nil {
+			return "", err
+		}
+
+		return string(bb), nil
+	}
+
+	bb, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return "", err
+	}
+	return string(bb), nil
+}
---
+++ cmd/influx/telegrafs.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"github.com/influxdata/influx-cli/v2/clients/telegrafs"
+	"github.com/influxdata/influx-cli/v2/pkg/cli/middleware"
+	"github.com/urfave/cli/v2"
+	"strings"
+)
+
+func newTelegrafsCommand() *cli.Command {
+	var params telegrafs.TelegrafParams
+	flags := append(commonFlags(), getOrgFlags(&params.OrgParams)...)
+	flags = append(flags, &cli.StringFlag{
+		Name:        "id",
+		Usage:       "Telegraf configuration ID to retrieve",
+		Aliases:     []string{"i"},
+		Destination: &params.Id,
+	})
+	return &cli.Command{
+		Name:  "telegrafs",
+		Usage: "List Telegraf configuration(s). Subcommands manage Telegraf configurations.",
+		Subcommands: []*cli.Command{
+			newCreateTelegrafCmd(),
+			newRemoveTelegrafCmd(),
+			newUpdateTelegrafCmd(),
+		},
+		Flags: flags,
+		Action: func(ctx *cli.Context) error {
+			api := getAPI(ctx)
+			client := telegrafs.Client{
+				CLI:          getCLI(ctx),
+				TelegrafsApi: api.TelegrafsApi,
+			}
+			return client.Telegrafs(ctx.Context, &params)
+		},
+	}
+}
+
+func newCreateTelegrafCmd() *cli.Command {
+	var params telegrafs.TelegrafParams
+	flags := append(commonFlags(), getOrgFlags(&params.OrgParams)...)
+	flags = append(flags, []cli.Flag{
+		&cli.StringFlag{
+			Name:        "description",
+			Usage:       "Description for Telegraf configuration",
+			Aliases:     []string{"d"},
+			Destination: &params.Desc,
+		},
+		&cli.StringFlag{
+			Name:        "file",
+			Usage:       "Path to Telegraf configuration",
+			Aliases:     []string{"f"},
+			Destination: &params.File,
+		},
+		&cli.StringFlag{
+			Name:        "name",
+			Usage:       "Name of Telegraf configuration",
+			Aliases:     []string{"n"},
+			Destination: &params.Name,
+		},
+	}...)
+	return &cli.Command{
+		Name:   "create",
+		Usage:  "The telegrafs create command creates a new Telegraf configuration.",
+		Flags:  flags,
+		Before: middleware.WithBeforeFns(withCli(), withApi(true)),
+		Action: func(ctx *cli.Context) error {
+			api := getAPI(ctx)
+			client := telegrafs.Client{
+				CLI:          getCLI(ctx),
+				TelegrafsApi: api.TelegrafsApi,
+			}
+			return client.Create(ctx.Context, &params)
+		},
+	}
+}
+
+func newRemoveTelegrafCmd() *cli.Command {
+	var params telegrafs.TelegrafParams
+	flags := append(commonFlags())
+	flags = append(flags, &cli.StringSliceFlag{
+		Name:        "id",
+		Usage:       "Telegraf configuration ID(s) to remove",
+		Aliases:     []string{"i"},
+	})
+	return &cli.Command{
+		Name:    "rm",
+		Usage:   "The telegrafs rm command removes Telegraf configuration(s).",
+		Aliases: []string{"remove"},
+		Flags:   flags,
+		Before:  middleware.WithBeforeFns(withCli(), withApi(true)),
+		Action: func(ctx *cli.Context) error {
+
+			// The old CLI allowed specifying this either via repeated flags or
+			// via a single flag w/ a comma-separated value.
+			rawIds := ctx.StringSlice("id")
+			var ids []string
+			for _, p := range rawIds {
+				ids = append(ids, strings.Split(p, ",")...)
+			}
+			params.Ids = ids
+
+			api := getAPI(ctx)
+			client := telegrafs.Client{
+				CLI:          getCLI(ctx),
+				TelegrafsApi: api.TelegrafsApi,
+			}
+			return client.Remove(ctx.Context, &params)
+		},
+	}
+}
+
+func newUpdateTelegrafCmd() *cli.Command {
+	var params telegrafs.TelegrafParams
+	flags := append(commonFlags(), getOrgFlags(&params.OrgParams)...)
+	flags = append(flags, []cli.Flag{
+		&cli.StringFlag{
+			Name:        "description",
+			Usage:       "Description for Telegraf configuration",
+			Aliases:     []string{"d"},
+			Destination: &params.Desc,
+		},
+		&cli.StringFlag{
+			Name:        "file",
+			Usage:       "Path to Telegraf configuration",
+			Aliases:     []string{"f"},
+			Destination: &params.File,
+		},
+		&cli.StringFlag{
+			Name:        "name",
+			Usage:       "Name of Telegraf configuration",
+			Aliases:     []string{"n"},
+			Destination: &params.Name,
+		},
+		&cli.StringFlag{
+			Name:        "id",
+			Usage:       "Telegraf configuration ID to retrieve",
+			Aliases:     []string{"i"},
+			Destination: &params.Id,
+		},
+	}...)
+	return &cli.Command{
+		Name:   "update",
+		Usage:  "Update a Telegraf configuration.",
+		Flags:  flags,
+		Before: middleware.WithBeforeFns(withCli(), withApi(true)),
+		Action: func(ctx *cli.Context) error {
+			api := getAPI(ctx)
+			client := telegrafs.Client{
+				CLI:          getCLI(ctx),
+				TelegrafsApi: api.TelegrafsApi,
+			}
+			return client.Update(ctx.Context, &params)
+		},
+	}
+}
