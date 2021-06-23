8972d23024dff69c8e62f0c75217eaf4a3f06f73
Daniel Moran <danxmoran@gmail.com>
2021-06-23 13:37:46 -0400

    feat: port jsonnet decoder from influxdb (#143)

--- go.mod
+++ go.mod
@@ -11,13 +11,12 @@ require (
 	github.com/gocarina/gocsv v0.0.0-20210408192840-02d7211d929d
 	github.com/golang/mock v1.5.0
 	github.com/google/go-cmp v0.5.5
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/google/go-jsonnet v0.17.0
 	github.com/mattn/go-isatty v0.0.13
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/text v0.3.3
 	golang.org/x/tools v0.1.0
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
 	honnef.co/go/tools v0.1.3
 )
---
+++ pkg/jsonnet/decode_test.go
@@ -0,0 +1,46 @@
+package jsonnet_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/influxdata/influx-cli/v2/pkg/jsonnet"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDecoder(t *testing.T) {
+	type (
+		person struct {
+			Name    string `json:"name"`
+			Welcome string `json:"welcome"`
+		}
+
+		persons struct {
+			Person1 person `json:"person1"`
+			Person2 person `json:"person2"`
+		}
+	)
+
+	const entry = `{
+  person1: {
+    name: "Alice",
+    welcome: "Hello " + self.name + "!",
+  },
+  person2: self.person1 { name: "Bob" },
+}`
+
+	var out persons
+	require.NoError(t, jsonnet.NewDecoder(strings.NewReader(entry)).Decode(&out))
+
+	expected := persons{
+		Person1: person{
+			Name:    "Alice",
+			Welcome: "Hello Alice!",
+		},
+		Person2: person{
+			Name:    "Bob",
+			Welcome: "Hello Bob!",
+		},
+	}
+	require.Equal(t, expected, out)
+}
