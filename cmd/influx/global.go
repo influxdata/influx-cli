81de7e63dd2649cd043c7a66c6698a9f5609552c
Sam Lai <70988+slai@users.noreply.github.com>
2021-10-22 15:06:12 +0100

    feat: return error if API token required but not found (#310)

--- cmd/influx/global.go
+++ cmd/influx/global.go
@@ -85,6 +85,9 @@ func newApiClient(ctx *cli.Context, configSvc config.Service, injectToken bool) 
 	configParams.Host = parsedHost
 
 	if injectToken {
+		if cfg.Token == "" {
+			return nil, fmt.Errorf("API token required. Pass `--token`, set `INFLUX_TOKEN`, or use `influx config set --token` to specify your token value")
+		}
 		configParams.Token = &cfg.Token
 	}
 	if ctx.IsSet(traceIdFlagName) {
