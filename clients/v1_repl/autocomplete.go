a1b097e0ac74b6a35e5f6481f865e15b5891c3bb
Andrew Lee <candrewlee14@gmail.com>
2022-06-10 20:35:11 -0600

    improve autocompletion with leftover handling

--- clients/v1_repl/autocomplete.go
+++ clients/v1_repl/autocomplete.go
@@ -11,20 +11,22 @@ import (
 
 type SuggestNode struct {
 	Description  string
-	subsuggestFn func(string) map[string]SuggestNode
+	subsuggestFn func(string) (map[string]SuggestNode, string)
 }
 
-func (c *Client) suggestUse(remainder string) map[string]SuggestNode {
+func (c *Client) suggestUse(remainder string) (map[string]SuggestNode, string) {
 	s := map[string]SuggestNode{}
 	for _, db := range c.Databases {
 		s["\""+db+"\""] = SuggestNode{Description: "Table Name"}
 	}
-	return s
+	return s, remainder
 }
 
-func (c *Client) suggestSelect(remainder string) map[string]SuggestNode {
+func (c *Client) suggestSelect(remainder string) (map[string]SuggestNode, string) {
 	s := map[string]SuggestNode{}
-	if isMatch, _ := regexp.Match(`(?i)FROM `+identRegex("from_clause")+`?$`, []byte(remainder)); isMatch {
+	fromReg := regexp.MustCompile(`(?i)FROM(\s+)` + identRegex("from_clause") + `?$`)
+	matches := reSubMatchMap(fromReg, remainder)
+	if matches != nil {
 		if c.Database != "" && c.RetentionPolicy != "" {
 			for _, m := range c.Measurements {
 				s["\""+m+"\""] = SuggestNode{Description: fmt.Sprintf("Measurement on \"%s\".\"%s\"", c.Database, c.RetentionPolicy)}
@@ -38,33 +40,39 @@ func (c *Client) suggestSelect(remainder string) map[string]SuggestNode {
 		for _, db := range c.Databases {
 			s["\""+db+"\""] = SuggestNode{Description: "Table Name"}
 		}
+		return s, getIdentFromMatches(matches, "from_clause")
 	}
-	return s
+	return s, remainder
 }
 
-func getSuggestions(remainder string, s map[string]SuggestNode) []prompt.Suggest {
+func getSuggestions(remainder string, s map[string]SuggestNode) ([]prompt.Suggest, string) {
 	// if remainder == "" {
 	// 	return []prompt.Suggest{}
 	// }
-	firstWord, rest, found := strings.Cut(remainder, " ")
-	if found {
+	firstWord, rest, gotWord := strings.Cut(remainder, " ")
+	leftOver := firstWord
+	if gotWord {
 		if node, found := s[strings.ToLower(firstWord)]; found {
 			if node.subsuggestFn == nil {
-				return []prompt.Suggest{}
+				return []prompt.Suggest{}, rest
 			}
-			return getSuggestions(rest, node.subsuggestFn(rest))
+			sugs, rem := node.subsuggestFn(rest)
+			return getSuggestions(rem, sugs)
 		} else if node, found := s[strings.ToUpper(firstWord)]; found {
 			if node.subsuggestFn == nil {
-				return []prompt.Suggest{}
+				return []prompt.Suggest{}, rest
 			}
-			return getSuggestions(rest, node.subsuggestFn(rest))
+			sugs, rem := node.subsuggestFn(rest)
+			return getSuggestions(rem, sugs)
+		} else if rest == "" {
+			leftOver = remainder
 		}
 	}
 	var sugs []prompt.Suggest
 	for text, node := range s {
 		sugs = append(sugs, prompt.Suggest{Text: text, Description: node.Description})
 	}
-	return sugs
+	return prompt.FilterFuzzy(sugs, leftOver, true), leftOver
 }
 
 func (c *Client) completer(d prompt.Document) []prompt.Suggest {
@@ -72,7 +80,7 @@ func (c *Client) completer(d prompt.Document) []prompt.Suggest {
 	suggestions := map[string]SuggestNode{
 		"use":    {Description: "Set current database", subsuggestFn: c.suggestUse},
 		"pretty": {Description: "Toggle pretty print for the json format"},
-		"precision": {Description: "Specify the format of the timestamp", subsuggestFn: func(rem string) map[string]SuggestNode {
+		"precision": {Description: "Specify the format of the timestamp", subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				"rfc3339": {},
 				"ns":      {},
@@ -81,34 +89,34 @@ func (c *Client) completer(d prompt.Document) []prompt.Suggest {
 				"s":       {},
 				"m":       {},
 				"h":       {},
-			}
+			}, rem
 		}},
 		"history":  {Description: "Display shell history"},
 		"settings": {Description: "Output the current shell settings"},
-		"clear": {Description: "Clears settings such as database", subsuggestFn: func(rem string) map[string]SuggestNode {
+		"clear": {Description: "Clears settings such as database", subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				"db":               {},
 				"database":         {},
 				"retention policy": {},
 				"rp":               {},
-			}
+			}, rem
 		}},
 		"exit":   {Description: "Exit the InfluxQL shell"},
 		"quit":   {Description: "Exit the InfluxQL shell"},
 		"gopher": {Description: "Display the Go Gopher"},
 		"help":   {Description: "Display help options"},
-		"format": {Description: "Specify the data display format", subsuggestFn: func(rem string) map[string]SuggestNode {
+		"format": {Description: "Specify the data display format", subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				"column": {},
 				"csv":    {},
 				"json":   {},
-			}
+			}, rem
 		}},
 		"SELECT":      {subsuggestFn: c.suggestSelect},
 		"INSERT":      {},
 		"INSERT INTO": {},
 		"DELETE":      {},
-		"SHOW": {subsuggestFn: func(rem string) map[string]SuggestNode {
+		"SHOW": {subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				// "CONTINUOUS QUERIES":            {},
 				"DATABASES": {},
@@ -133,18 +141,18 @@ func (c *Client) completer(d prompt.Document) []prompt.Suggest {
 				"TAG KEYS":                  {},
 				"TAG VALUES":                {},
 				"USERS":                     {},
-			}
+			}, rem
 		}},
-		"CREATE": {subsuggestFn: func(rem string) map[string]SuggestNode {
+		"CREATE": {subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				"CONTINUOUS QUERY": {},
 				// "DATABASE":         {},
 				"USER":             {},
 				"RETENTION POLICY": {},
 				"SUBSCRIPTION":     {},
-			}
+			}, rem
 		}},
-		"DROP": {subsuggestFn: func(rem string) map[string]SuggestNode {
+		"DROP": {subsuggestFn: func(rem string) (map[string]SuggestNode, string) {
 			return map[string]SuggestNode{
 				"CONTINUOUS QUERY": {},
 				"DATABASE":         {},
@@ -154,7 +162,7 @@ func (c *Client) completer(d prompt.Document) []prompt.Suggest {
 				"SHARD":            {},
 				"SUBSCRIPTION":     {},
 				"USER":             {},
-			}
+			}, rem
 		}},
 		"EXPLAIN":         {},
 		"EXPLAIN ANALYZE": {},
@@ -165,9 +173,9 @@ func (c *Client) completer(d prompt.Document) []prompt.Suggest {
 		// "KILL QUERY":       {},
 	}
 	line := d.CurrentLineBeforeCursor()
-	currentSuggestions := getSuggestions(line, suggestions)
+	currentSuggestions, _ := getSuggestions(line, suggestions)
 	sort.Slice(currentSuggestions, func(i, j int) bool {
 		return strings.ToLower(currentSuggestions[i].Text) < strings.ToLower(currentSuggestions[j].Text)
 	})
-	return prompt.FilterFuzzy(currentSuggestions, d.GetWordBeforeCursor(), true)
+	return currentSuggestions
 }
