0fd75c8b7ba35b56672d4c03fc61609b49ed400c
Jamie Strandboge <jamie@influxdata.com>
2026-02-10 11:42:56 -0600

    chore: update scripts/ci/install-go.sh to use 1.25.7

--- scripts/ci/install-go.sh
+++ scripts/ci/install-go.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-declare -r GO_VERSION=1.19.6
+declare -r GO_VERSION=1.25.7
 
 # Hashes are from the table at https://golang.org/dl/
 function go_hash () {
   case $1 in
     linux_amd64)
-      echo e3410c676ced327aec928303fef11385702a5562fd19d9a1750d5a2979763c3d
+      echo 12e6d6a191091ae27dc31f6efc630e3a3b8ba409baf3573d955b196fdf086005
       ;;
     linux_arm64)
-      echo e4d63c933a68e5fad07cab9d12c5c1610ce4810832d47c44314c3246f511ac4f
+      echo ba611a53534135a81067240eff9508cd7e256c560edd5d8c2fef54f083c07129
       ;;
     mac)
-      echo 63386d51c69cef6c001ff0436832289635ba4a2649282451a18827e93507b444
+      echo bf5050a2152f4053837b886e8d9640c829dbacbc3370f913351eb0904cb706f5
       ;;
     windows)
-      echo 8d84af29e46c38b1eec77f9310310517c9e394ac7489e1c7329a94b443b0388d
+      echo c75e5f4ff62d085cc0017be3ad19d5536f46825fa05db06ec468941f847e3228
       ;;
   esac
 }
